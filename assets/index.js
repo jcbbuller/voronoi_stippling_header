f9ef341a7187e66bf8715d62e5ea33e893b6c4b5
jcbbuller <39416698+jcbbuller@users.noreply.github.com>
2024-03-21 15:17:58 -0400

    Added initial files
    Added the initial file examples of using Voronoi Stippling for a cool artistic effect on your website or application.

---
+++ assets/index.js
@@ -0,0 +1,1 @@
+export {default} from "./dottedheader_1.js";
---
+++ assets/scripts.js
@@ -0,0 +1,9 @@
+
+  
+  /*
+    Slidemenu
+  */
+  function toggleMenu() {
+    var element = document.body;
+    element.classList.toggle("menu-active");
+   }
